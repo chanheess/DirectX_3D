65747a5c87f3ba5218b82d7de36a57b6f8faaa63
chanheess <kim759900@naver.com>
2021-12-15 16:23:47 +0900

    change directory

--- 2D_FreeStyle1/FrameWork/Components/Bounding/ILine.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct ILine
-{
-	virtual void GetLine(vector<Vertex>* vertices, D3DXMATRIX world) = 0;
-};
--- 2D_FreeStyle1/FrameWork/GameObjects/CameraObj.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-class CameraObj : public GameObject
-{
-public:
-	CameraObj(wstring name =L"Camera");
-	~CameraObj();
-};
-
